e69159aa7324d4ff8b15b4048c774b4a382b4807
Akshay Ayyanchira <ayyanchira.akshay@gmail.com>
2022-07-17 13:02:00 -0700

    Initial Stage of the app

---
+++ app/src/main/java/com/ideas2app/tiptime/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.ideas2app.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.ideas2app.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val cost = binding.costOfService.text.toString().toDoubleOrNull()
+        if(cost == null) {
+            binding.tipResult.text = ""
+            Toast.makeText(this, "Cost of service cannot be empty.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_fifteen_percent -> 0.15
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.20
+        }
+
+        val tip = tipPercentage * cost
+        var total:Double = cost + tip
+        if (binding.roundUpSwitch.isChecked) {
+            total = kotlin.math.ceil(total)
+        }
+
+        val formattedCost = NumberFormat.getCurrencyInstance().format(total)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedCost)
+    }
+}
