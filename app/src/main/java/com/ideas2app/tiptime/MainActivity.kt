d3cd1cd68acd17e5fbb1a61a5eb6318d294e515e
Akshay Ayyanchira <ayyanchira.akshay@gmail.com>
2022-08-17 02:23:12 -0700

    Completed polishing the app

--- app/src/main/java/com/ideas2app/tiptime/MainActivity.kt
+++ app/src/main/java/com/ideas2app/tiptime/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.ideas2app.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.ideas2app.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -17,12 +20,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
     }
 
     private fun calculateTip() {
         val cost = binding.costOfServiceEditText.text.toString().toDoubleOrNull()
         binding.tipResult.visibility = View.VISIBLE
-        if(cost == null) {
+        if (cost == null) {
             binding.tipResult.text = ""
             Toast.makeText(this, "Cost of service cannot be empty.", Toast.LENGTH_SHORT).show()
             return
@@ -35,12 +41,24 @@ class MainActivity : AppCompatActivity() {
         }
 
         val tip = tipPercentage * cost
-        var total:Double = cost + tip
+        var total: Double = cost + tip
         if (binding.roundUpSwitch.isChecked) {
             total = kotlin.math.ceil(total)
         }
 
         val formattedCost = NumberFormat.getCurrencyInstance().format(total)
         binding.tipResult.text = getString(R.string.tip_amount, formattedCost)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            calculateTip()
+            return true
+        }
+        return false
     }
 }
